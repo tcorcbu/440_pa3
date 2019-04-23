e12d5811325333568d2d81108aa128a533b14cba
Tom Corcoran <tom.corcoran@volossoftware.com>
2019-04-23 00:50:58 -0400

    adding structure for creating tree, creating node class

---
+++ Node.java
@@ -0,0 +1,9 @@
+public class Node {
+
+	public static List<Node> children;
+	public static List<positionTicTacToe> board;
+	public static int heuristic_value;
+
+	public Node(){}
+
+}
--- aiTicTacToe.java
+++ aiTicTacToe.java
@@ -25,6 +25,117 @@ public class aiTicTacToe {
 			
 		
 	}
+
+
+	// Make node class
+	// node has:
+	//	heuristic value
+	//	children
+	//	board object?
+	//	isTerminal function
+	//	getChildren function
+	//	getHeuristic function
+
+	private Node createTree(int depth, List<positionTicTacToe> board){
+
+		// create node and init with current board
+		Node node = Node();
+		node.board = board;
+
+		createChildren(node, depth);
+
+		return node;
+
+		
+	}
+
+	private Node createChildren(Node node, int depth){
+
+		// loop over possible moves and create Node for each move and 
+		// init board with that position added
+
+		// append each node to the first node's children List
+
+		// when looping over every node, do the same thing for its children
+
+		// repeat this according to the depth 
+
+		if(depth == 0){ // when the depth is reached the max (leaf node) compute that Node's board's heuristic 
+			node.heuristic_value = getHeuristic(node.board);
+			return node;
+		}
+
+		// get possible moves (x,y,z) for the X player or O player
+		int[] possible_positions = getMoves(node.board); //CHANGE
+
+		for(int i = 0; i < possible_positions.length; i++){
+
+			Node child = Node();
+			List<positionTicTacToe> possible_move_board = addPosToBoard(possible_positions[i], node.board);
+			child.board = possible_move_board;
+
+			Node child_node = createChildren(child, depth - 1);
+
+			node.children.append(child_node);
+
+		}
+
+		return node;
+
+	}
+
+
+	private int minimax(Node node, int depth, int alpha, int beta, boolean maximizingPlayer){
+
+		if(depth == 0 node.isTerminal()){ //depth = 0 or node is a terminal node then
+			//return the heuristic value of node
+			return node.value;
+		}
+
+	    if(maximizingPlayer){
+
+	    	int value = Integer.MIN_VALUE;// −infinity
+	    	int rec = 0;
+
+	    	Node[] children = node.getChildren();
+
+	        for(int i = 0; i < children.length; i++){
+	        	rec = minimax(children[i], depth − 1, alpha, beta, false)
+	        	value = Math.max(value, rec);
+	        	alpha = Math.max(alpha, rec);
+	        	if (beta <= alpha){
+	        		break;
+	        	}
+	        }
+
+	        return value;
+
+	    } else { // (* minimizing player *)
+
+	    	int value = Integer.MAX_VALUE;// infinity
+	    	int rec = 0;
+
+	    	Node[] children = node.getChildren();
+
+	        for(int i = 0; i < children.length; i++){
+	        	rec = minimax(children[i], depth − 1, alpha, beta, true)
+	        	value = Math.min(value, rec);
+	        	beta = Math.min(beta, rec);
+	        	if (beta <= alpha){
+        			break;
+	        	}
+	        }
+
+	        return value;
+
+	    }
+
+	}
+
+    
+
+
+
 	private List<List<positionTicTacToe>> initializeWinningLines()
 	{
 		//create a list of winning line so that the game will "brute-force" check if a player satisfied any 	winning condition(s).
